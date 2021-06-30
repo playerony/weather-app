43133474477d5303fe8e30ad6608b41c73a61963
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2021-06-30 16:30:06 +0200

    🔧 Configure styled-components (#7)

---
+++ src/infrastructure/styles/index.ts
@@ -0,0 +1,4 @@
+export * from './atoms';
+export * from './theme';
+export * from './mixins';
+export * from './variables';
---
+++ src/infrastructure/styles/variables/typography.ts
@@ -0,0 +1,36 @@
+export const fontFamily = '"Montserrat", "Raleway", sans-serif';
+
+export const typeScale = {
+  h1: '2.0rem',
+  h2: '1.8rem',
+  h3: '1.6rem',
+  h4: '1.4rem',
+  h5: '1.2rem',
+  paragraph: '1.1rem',
+  helperText: '0.9rem',
+  copyrightText: '0.8rem',
+};
+
+export const fontWeight = {
+  bold: 700,
+  semibold: 600,
+  normal: 500,
+  regular: 400,
+  light: 300,
+};
+
+export const spacing = {
+  xsmall: '5px',
+  small: '10px',
+  xmedium: '15px',
+  medium: '20px',
+  xbase: '30px',
+  base: '40px',
+  xlarge: '50px',
+  large: '60px',
+};
+
+export const radius = {
+  default: '5px',
+  medium: '10px',
+};
